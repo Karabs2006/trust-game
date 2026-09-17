7e9aa5190cb4e97e189726c17bb90aa828e6434d
SkullManea <venturousskull@gmail.com>
2026-09-17 18:33:40 +0200

    Attempt to get interactions working

--- trust-game/Assets/Scripts/NPC/DialogueController.cs
+++ trust-game/Assets/Scripts/NPC/DialogueController.cs
@@ -14,6 +14,7 @@ public class DialogueController : MonoBehaviour, IInteractable
 
     [HideInInspector] public bool isDialogueActive;
 
+
     void Start()
     {
         dialogueUI = DialogueUI.Instance;
--- trust-game/Assets/Scripts/NPC/NPCEvent.cs
+++ trust-game/Assets/Scripts/NPC/NPCEvent.cs
@@ -3,24 +3,39 @@ using UnityEngine;
 public class NPCEvent : MonoBehaviour
 {
     public DialogueController dialogueController;
+    public PlayerMovement playerMovement;
 
-    void Start()
+    void FixedUpdate()
     {
-        
+        StateManager();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            //Stop Player Movement when puzzle is active
-            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
-
+            //Stop Player Movement when dialogue is active
             playerMovement.currentSpeed = 0;
 
             //calls dialogue
             dialogueController.StartDialogue();
         }
     }
+
+    public void Test()
+    {
+        dialogueController.Interact(); 
+
+    }
+
+    public void StateManager()
+    {
+        if (!dialogueController.isDialogueActive)
+        {
+            // Debug.Log ("done");
+            // playerMovement.currentSpeed = 10f;
+        }
+    }
+
 
   
 }
